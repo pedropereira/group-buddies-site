1008eb6334d255cb724420ccb0bcc4f54c0c1048
André Santos <andrefs@cpan.org>
2012-07-17 19:15:29 +0100

    require_relative fallback for ruby 1.8.7

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@ require 'pony'
 require 'sass'
 require 'sinatra'
 
+# require_relative does not exist in ruby 1.8.7
+# This is a fallback -- http://stackoverflow.com/a/4718414/951432
+unless Kernel.respond_to?(:require_relative)
+  module Kernel
+    def require_relative(path)
+      require File.join(File.dirname(caller[0]), path.to_str)
+    end
+  end
+end
+
 require_relative 'helpers/helpers'
 
 set :sass, :style => :compressed
