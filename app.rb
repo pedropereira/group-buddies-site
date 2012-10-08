0ee9867ad98257a0833bdd5c65fff8bd9b44e843
Zamith <zamith.28@gmail.com>
2012-10-08 14:37:32 +0100

    forcing the user of smtp

--- app.rb
+++ app.rb
@@ -57,7 +57,16 @@ post '/contact' do
             :reply_to => params[:email],
             :subject => '[groupbuddies.com] Message from ' + params[:name],
             :body => params[:message],
-            :via => :sendmail
+            :via => :smtp,
+            :via_options => {
+              :address              => 'smtp.gmail.com',
+              :port                 => '587',
+              :enable_starttls_auto => true,
+              :user_name            => 'noreply@groupbuddies.com',
+              :password             => 'noreplygbsuperpass',
+              :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+              :domain               => "groupbuddies.com" # the HELO domain provided by the client to the server
+            }
 
   redirect to('/') unless request.xhr?
 end
